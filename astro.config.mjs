f4b67375d4f687f84d2f563fd6226d65ea66bc65
zaer1n <105508453+zaer1n@users.noreply.github.com>
2024-11-13 19:19:11 +0300

    fix: rename `other` dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,7 +67,7 @@ export default defineConfig({
             tr: "Bilgi",
             ru: "Информация",
           },
-          autogenerate: { directory: "other" },
+          autogenerate: { directory: "info" },
         },
       ],
     }),
