f2b05cadc7cc21239a029012d408063f961eed97
Avolicious <avolicious@kog.tw>
2024-11-13 16:57:20 +0100

    fix: some chores to the release changelog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import starlightBlog from 'starlight-blog'
+import starlightBlog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
