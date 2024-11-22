62efce1beea08ee9a2d7279414a6d0d6870aaaff
Avolicious <avolicious@kog.tw>
2024-11-22 19:31:19 +0100

    enh: changed stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,72 +5,78 @@ import starlightBlog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "KoG Community",
-      customCss: ["./src/styles/custom.css"],
-      components: {},
-      favicon: "/tee_light.svg",
-      logo: {
-        dark: "./src/assets/tee_light.svg",
-        light: "./src/assets/tee_dark.svg",
+  vite: {
+    build: {
+      modulePreload: true,
+      cssMinify: "lightningcss",
+      minify: "terser",
+      rollupOptions: {}
+    }
+  },
+  integrations: [starlight({
+    title: "KoG Community",
+    customCss: ["./src/styles/custom.css"],
+    components: {},
+    favicon: "/tee_light.svg",
+    logo: {
+      dark: "./src/assets/tee_light.svg",
+      light: "./src/assets/tee_dark.svg",
+    },
+    social: {
+      discord: "https://discord.kog.tw",
+      github: "https://github.com/KoG-teeworlds/community",
+    },
+    plugins: [
+      starlightBlog({
+        authors: {},
+      })
+    ],
+    defaultLocale: "root",
+    locales: {
+      root: {
+        label: "English",
+        lang: "en",
       },
-      social: {
-        discord: "https://discord.kog.tw",
-        github: "https://github.com/KoG-teeworlds/community",
+      ru: {
+        label: "Русский",
       },
-      plugins: [
-        starlightBlog({
-          authors: {},
-        })
-      ],
-      defaultLocale: "root",
-      locales: {
-        root: {
-          label: "English",
-          lang: "en",
-        },
-        ru: {
-          label: "Русский",
-        },
-        tr: {
-          label: "Türkçe",
-        },
+      tr: {
+        label: "Türkçe",
       },
-      sidebar: [
-        {
-          label: "Community",
-          translations: {
-            tr: "Topluluk",
-            ru: "Сообщество",
-          },
-          autogenerate: { directory: "community" },
+    },
+    sidebar: [
+      {
+        label: "Community",
+        translations: {
+          tr: "Topluluk",
+          ru: "Сообщество",
         },
-        {
-          label: "Projects",
-          translations: {
-            tr: "Projeler",
-            ru: "Проекты",
-          },
-          autogenerate: { directory: "projects" },
+        autogenerate: { directory: "community" },
+      },
+      {
+        label: "Projects",
+        translations: {
+          tr: "Projeler",
+          ru: "Проекты",
         },
-        {
-          label: "Guides",
-          translations: {
-            tr: "Rehberler",
-            ru: "Руководства",
-          },
-          autogenerate: { directory: "guides" },
+        autogenerate: { directory: "projects" },
+      },
+      {
+        label: "Guides",
+        translations: {
+          tr: "Rehberler",
+          ru: "Руководства",
         },
-        {
-          label: "Info",
-          translations: {
-            tr: "Bilgi",
-            ru: "Информация",
-          },
-          autogenerate: { directory: "other" },
+        autogenerate: { directory: "guides" },
+      },
+      {
+        label: "Info",
+        translations: {
+          tr: "Bilgi",
+          ru: "Информация",
         },
-      ],
-    }),
-  ],
+        autogenerate: { directory: "other" },
+      },
+    ],
+  })],
 });
