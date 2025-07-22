9a951a9c0e27756b08f76567f30cb1a81b52e97d
Avolicious <avolicious@kog.tw>
2025-07-22 12:54:42 +0200

    fix: docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlightBlog from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://community.kog.tw",
   vite: {
     build: {
       modulePreload: true,
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,11 @@ import { docsSchema } from "@astrojs/starlight/schema";
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { blogSchema } from "starlight-blog/schema";
 
-const docs = defineCollection({ loader: docsLoader(), schema: docsSchema() });
-const blog = defineCollection({
-  loader: docsLoader(),
-  schema: docsSchema({
-    extend: (context) => blogSchema(context),
+export const collections = {
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: (context) => blogSchema(context),
+    }),
   }),
-});
-
-export const collections = { docs, blog };
+};
